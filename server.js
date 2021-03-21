11e6111719eca7bc17bc2a6c75d64aa17c81b34f
Fabian Vis <70572501+fabian-vis@users.noreply.github.com>
2021-03-21 21:35:16 +0100

    Revert "Added an add feature to the project"
    This reverts commit 4e2e7b587058df505633c060a9ac046e72ff0524.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const connectDB = require('./model/db.js')
 const User = require("./model/user.js")
 const steamFetch = require("./route/steamFetch.js")
 const profile = require("./route/profile.js")
-const likeFeature = require("./route/likeFeature.js")
+const likeFeature = require("./route/likeFeature")
 
 // call on db connection module
 connectDB();
