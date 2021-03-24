e32a681ab81f7b1a4c58156d6e2d1571d292c950
maggness <timderoller@gmail.com>
2021-03-24 13:35:26 +0100

    css update, borders & sides

--- route/profile.js
+++ route/profile.js
@@ -3,13 +3,13 @@ const profile = express.Router()
 const scheme = require('../model/user')
 
 profile.get('/profile/:username', (req, res) => {
-    scheme.findOne({
-        username: req.params.username
-    }).then(results => {
-        res.render('layouts/profile', {
-            profile: results
-        })
+  scheme.findOne({
+    username: req.params.username
+  }).then(results => {
+    res.render('layouts/profile', {
+      profile: results
     })
+  })
 })
 
 module.exports = profile;
--- route/steamFetch.js
+++ route/steamFetch.js
@@ -5,50 +5,60 @@ const User = require("../model/user.js")
 const SteamAPI = require('steamapi');
 const steam = new SteamAPI(process.env.STEAMAPI);
 
-let now  = new Date();
+let now = new Date();
 let day = 1; // er staat nu 1, maar dit moet now.getDay() worden om de dag te checken
 
 console.log(day);
 
 router.get('/', (req, res) => {
-  if ( day == 1 ) {
-    const { error } = req.query
+  if (day == 1) {
+    const {
+      error
+    } = req.query
     if (error && error === 'No match') {
-        res.render('layouts/index', { message: 'User not found' })
-        return
+      res.render('layouts/index', {
+        message: 'User not found'
+      })
+      return
     }
-    res.render('layouts/index', { message: '' })
+    res.render('layouts/index', {
+      message: ''
+    })
   } else {
     res.render('layouts/notMonday.ejs')
   }
 })
 
 router.post('/login', (req, res) => {
-    const { id: steamUser } = req.body
-    const gameGenre = req.body.gameGenre
-    User.findOne({username: steamUser}).then(result => {
-        if (!result) {
-            steam.resolve(steamUser).then(id => {
-                steam.getUserSummary(id).then(userSummary => {
-                    new User({
-                        username: steamUser,
-                        steamID: userSummary.steamID,
-                        country: userSummary.countryCode || steamUser + ' has decided to not share this information',
-                        profilePicture: userSummary.avatar.medium,
-                        gameGenre: gameGenre
-                    })
-                    .save()
-                    .then(result =>
-                        res.redirect('/profile/' + result.username)
-                    )
-                })
-            }).catch(err => {
-                res.redirect('/?error=' + err.message)
+  const {
+    id: steamUser
+  } = req.body
+  const gameGenre = req.body.gameGenre
+  User.findOne({
+    username: steamUser
+  }).then(result => {
+    if (!result) {
+      steam.resolve(steamUser).then(id => {
+        steam.getUserSummary(id).then(userSummary => {
+          new User({
+              username: steamUser,
+              steamID: userSummary.steamID,
+              country: userSummary.countryCode || steamUser + ' has decided to not share this information',
+              profilePicture: userSummary.avatar.medium,
+              gameGenre: gameGenre
             })
-            return
-        }
-        res.redirect('/profile/' + result.username)
-    })
+            .save()
+            .then(result =>
+              res.redirect('/profile/' + result.username)
+            )
+        })
+      }).catch(err => {
+        res.redirect('/?error=' + err.message)
+      })
+      return
+    }
+    res.redirect('/profile/' + result.username)
+  })
 })
 
 module.exports = router;
