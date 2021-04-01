88cb6cf32aa71a4b7acfff2f8b0ee48fe7c34570
maggness <timderoller@gmail.com>
2021-04-01 12:36:20 +0200

    nav fix, no more onclick (dont kill me)

--- static/script/navigatie.js
+++ static/script/navigatie.js
@@ -1,3 +1,6 @@
+const forwardnavbutton = document.querySelector('#forwardnav');
+const backwardnavbutton = document.querySelector('#backwardnav');
+
 // script for the page nav
 function goForward() {
   event.preventDefault();
@@ -8,3 +11,6 @@ function goBack() {
   event.preventDefault();
   window.history.back();
 }
+
+forwardnavbutton.addEventListener("click", goForward);
+backwardnavbutton.addEventListener("click", goBack);
